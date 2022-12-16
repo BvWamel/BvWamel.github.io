44ab4aea8d54231619f040d058d989ceb3cfe3b4
Boaz Wamel <boaz_1999@live.nl>
2022-12-16 17:51:44 +0100

    Youtube clone
    Youtube clone made with React

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => {
+      setChannelDetail(data?.items[0]);
+    });
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => {
+        setVideos(data?.items);
+      }
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,37,1) 66%, rgba(201,45,65,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-127px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
